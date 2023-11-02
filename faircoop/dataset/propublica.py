01b6e059633bf755d991634a75720be54e66f847
Martijn de Vos <martijn.devos@epfl.ch>
2023-11-02 11:02:51 +0100

    Added ProPublica dataset

---
+++ faircoop/dataset/propublica.py
@@ -0,0 +1,21 @@
+import os
+
+import pandas as pd
+
+from faircoop.dataset.dataset import Dataset
+
+
+class ProPublicaDataset(Dataset):
+
+    def load_dataset(self):
+        self.logger.info("Loading ProPublica dataset...")
+        self.features = pd.read_csv(os.path.join("data", self.get_name(), "features.csv"))
+        self.labels = pd.read_csv(os.path.join("data", self.get_name(), "labels.csv"))
+        self.logger.info("ProPublica dataset loaded (rows: %d)", len(self.features))
+
+        self.protected_attributes = ["Female", "African_American"]
+        self.compute_ground_truth_dp()
+        self.compute_subspace_probabilities()
+
+    def get_name(self) -> str:
+        return "propublica"
